6befb1d931e24880d58d70d444f099d6bcec1fca
Ares42 <Luke Solomon>
2014-07-03 10:21:19 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -100,10 +100,7 @@ static const int GRID_COLUMNS = 10;
 
 -(void)countNeighbors {
     
-    /*
-     iterate through rows
-     note that NSArray has a method 'count' that will return the number of elements in the array
-     */
+    //iterate through row, note that NSArray has a method 'count' that will return the number of elements in the array
     for (int i = 0; i < [_gridArray count]; i++)
     {
         //iterate through all the columns for a given row
